77d5aadbef41644c0e2897636de35d6b4f1b5862
FREITAS DA MOTA RUI MANUEL <RUI.FRTSD@ge-pedago.etat-ge.ch>
2026-09-01 09:22:07 +0200

    ajout file s3

---
+++ ALP-S3/ALP-S3Ex1.py
@@ -0,0 +1,14 @@
+# Imports
+
+# Constantes
+
+# Procédures et fonctions
+
+# Procédure main()
+def main():
+    pass # à remplacer par vos instructions...
+    
+# Appel de la procédure main()
+if __name__ == "__main__":
+    main()
+
