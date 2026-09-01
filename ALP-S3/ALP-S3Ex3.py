7d1222ce374cb517a0c7a05abd72850689519be9
FREITAS DA MOTA RUI MANUEL <RUI.FRTSD@ge-pedago.etat-ge.ch>
2026-09-01 11:48:29 +0200

    fin ex3

--- ALP-S3/ALP-S3Ex3.py
+++ ALP-S3/ALP-S3Ex3.py
@@ -5,10 +5,10 @@ RABAIS = 0.1
 QUANTITE_PASS = 100
 # Procédures et fonctions
 def entree():
-    prix_unit = int(input("Entrez prix unitaire : "))
+    prix_unit = float(input("Entrez prix unitaire : "))
     quantite_unit = int(input("Entrez nombres de pièce(s) : "))
     prix_avant_rabais = prix_unit*quantite_unit
-    return prixUnit,quantiteUnit,prix_avant_rabais
+    return prix_unit,quantite_unit,prix_avant_rabais
 
 def calculer_rabais(quantite_unit, prix_avant_rabais):
     if quantite_unit <= QUANTITE_PASS :
@@ -17,18 +17,17 @@ def calculer_rabais(quantite_unit, prix_avant_rabais):
         taux = prix_avant_rabais * RABAIS
     return taux        
 
-def caca():
-    print("caca")
-def afficher(taux)
-    print("Le prix pour ", quantite_unit, "est :")
-    print("prix avant rabais : ", prixUnit*quantiteUnit)
+def afficher(prix_unit,quantite_unit,prix_avant_rabais,taux):
+    print("Pour ", quantite_unit, " pièces à ",prix_unit ,":")
+    print("prix avant rabais : ", prix_unit*quantite_unit)
     print("montant du rabais : ", taux)
-    print("prix à payer : ", prix_avant_rabais-rabais)
+    print("prix à payer : ", prix_avant_rabais-taux)
 
 # Procédure main()
 def main():
-    entre
-    
+    prix_unit,quantite_unit,prix_avant_rabais = entree()
+    taux = calculer_rabais(quantite_unit,prix_avant_rabais)
+    afficher(prix_unit,quantite_unit,prix_avant_rabais,taux)
 # Appel de la procédure main()
 if __name__ == "__main__":
     main()
