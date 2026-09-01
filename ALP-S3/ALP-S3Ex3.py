8972c76dd17496fa508590c9833d414f305bff2a
FREITAS DA MOTA RUI MANUEL <RUI.FRTSD@ge-pedago.etat-ge.ch>
2026-09-01 11:35:26 +0200

    ajout ex3

---
+++ ALP-S3/ALP-S3Ex3.py
@@ -0,0 +1,34 @@
+# Imports
+
+# Constantes
+RABAIS = 0.1
+QUANTITE_PASS = 100
+# Procédures et fonctions
+def entree():
+    prixUnit = int(input("Entrez prix unitaire : "))
+    quantiteUnit = int(input("Entrez nombres de pièce(s) : "))
+    prixAvantRabais = prixUnit*quantiteUnit
+    return prixAvantRabais
+
+def calculer_rabais(prixAvantRabais):
+    if quantiteUnit <= QUANTITE_PASS :
+        taux = 0
+    else :
+        taux = prixAvantRabais * RABAIS
+    return taux        
+
+
+def afficher(taux)
+    print("Le prix pour ", quantiteUnit, "est :")
+    print("prix avant rabais : ", prixUnit*quantiteUnit)
+    print("montant du rabais : ", taux)
+    print("prix à payer : ", prixAvantRabais-rabais)
+
+# Procédure main()
+def main():
+    entre
+    
+# Appel de la procédure main()
+if __name__ == "__main__":
+    main()
+
