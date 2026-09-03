535880adb971ec662b8a150c2ee4b3a8324a2d3d
FREITAS DA MOTA RUI MANUEL <RUI.FRTSD@ge-pedago.etat-ge.ch>
2026-09-03 10:44:26 +0200

    ajout s3 ex5 + s4 ex1

--- ALP-S3/ALP-S3Ex5.py
+++ ALP-S3/ALP-S3Ex5.py
@@ -17,13 +17,29 @@ def entree():
 
 def calculer_prix(nombre_piece, prix_piece):
     if nombre_piece >= 1000 :
-        rabais = RABAIS_1000P
+        rabais = RABAIS_1000P 
+    elif nombre_piece > 100 :
+        rabais = RABAIS_100P
+    else :
+        rabais = RABAIS_50P
+    if nombre_piece > 500 :
         tva = TVA_PLUS_500P
-    elif nombre_piece > 500 :
+    elif nombre_piece > 250 :
+        tva = TVA_MOINS_500P
+    else :
+        tva = TVA_MOINS_250P
+        
+    prix_post_modif = (nombre_piece*prix_piece)*(1-rabais)*tva    
+    
+    return rabais, tva,prix_post_modif
+
+def afficher(nombre_piece, prix_piece,rabais, tva, prix_post_modif):
+    print(f"Pour {nombre_piece} au prix unitaire de {prix_piece} et avec une reduction de {rabais*100}% et en prenant en compte la tva de {tva*100-100} le prix final sera de {prix_post_modif}")
 # Procédure main()
 def main():
     nombre_piece, prix_piece = entree()
-    
+    rabais, tva, prix_post_modif = calculer_prix(nombre_piece, prix_piece)
+    afficher(nombre_piece, prix_piece,rabais, tva, prix_post_modif)
 # Appel de la procédure main()
 if __name__ == "__main__":
     main()
---
+++ ALP-S4/ALP-S4Ex1.py
@@ -0,0 +1,20 @@
+# Imports
+
+# Constantes
+
+# Procédures et fonctions
+def entree():
+    type_piano = int(input("Quel est le type de piano [Piano droit -> 1 ou Piano à queue -> 2]"))
+    nombre_etage_monter = int(input("Quel est le nombre d'étage à monter"))
+    nombre_etage_descendre = int(input("Quel est le nombre d'étage à descendre"))
+    nombre_km = int(input("Quel est le nombre de kilometres à éffectuer"))
+    
+    return type_piano,nombre_etage_monter,nombre_etage_descendre,nombre_km
+# Procédure main()
+def main():
+    pass # à remplacer par vos instructions...
+    
+# Appel de la procédure main()
+if __name__ == "__main__":
+    main()
+
