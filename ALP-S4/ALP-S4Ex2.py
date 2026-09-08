ba9bb5b60a7334624b63af31550d783ec69d6aa5
FREITAS DA MOTA RUI MANUEL <RUI.FRTSD@ge-pedago.etat-ge.ch>
2026-09-08 10:57:26 +0200

    exs added

---
+++ ALP-S4/ALP-S4Ex2.py
@@ -0,0 +1,48 @@
+# Imports
+
+# Constantes
+SEUIL_5 = 5
+SEUIL_9 = 9
+PRIX_DEBOUT = 20
+PRIX_ASSISES = 30
+# Procédures et fonctions
+def entree():
+    place_debout = int(input("Combien de place debout :"))
+    place_assises = int(input("Combien de place assises :"))
+    
+    return place_debout,place_assises
+
+def calculer(place_debout,place_assises):
+    prix_final_debout = 0
+    prix_final_assises = 0
+    if place_debout >= SEUIL_9:
+        place_debout -= 2
+    elif place_debout >=SEUIL_5:
+        place_debout -= 1
+    
+    if place_assises >= SEUIL_9:
+        place_assises -= 2
+    elif place_assises >= SEUIL_5:
+        place_assises -= 1
+        
+        
+        #ne prend pas encompte toute la valeur
+    for i in range(place_assises):
+        prix_final_assises += PRIX_ASSISES
+    for i in range(place_debout):
+        prix_final_debout += PRIX_DEBOUT
+    
+    return prix_final_debout,prix_final_assises
+
+def afficher(place_debout,place_assises,prix_final_debout,prix_final_assises):
+    print(f"Le prix à payer pour {place_debout} places debout : {prix_final_debout} Frs")
+    print(f"Le prix à payer pour {place_assises} places assises : {prix_final_assises} Frs")
+# Procédure main()
+def main():
+    place_debout,place_assises = entree()
+    prix_final_debout,prix_final_assises = calculer(place_debout,place_assises)
+    afficher(place_debout,place_assises,prix_final_debout,prix_final_assises)
+    
+# Appel de la procédure main()
+if __name__ == "__main__":
+    main()
---
+++ ALP-S8/ALP-S8ExT1.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+for i in range(200):
+    if i % 2 == 0:
+        fd(10 + i)
+        right(90)
+    else:
+        fd(9 + i)
+        right(90)
+        
+        
+        
+        ##to develop
