f2c615ae15fe93afc5cf55d3ec8362633c7b35db
FREITAS DA MOTA RUI MANUEL <RUI.FRTSD@ge-pedago.etat-ge.ch>
2026-09-08 08:42:58 +0200

    terminer s4ex1

--- ALP-S4/ALP-S4Ex1.py
+++ ALP-S4/ALP-S4Ex1.py
@@ -16,24 +16,44 @@ def entree():
 
 def calculer(type_piano,nombre_etage_monter,nombre_etage_descendre,nombre_km):
    tarif_total_km = 0
-   if type_piano == 1 : #piano droit
+   tarif_total_etage = 0
+   
+   if type_piano == 1: #piano droit
         tarif_piano = TARIF_PIANO_DROIT
-    elif type_piano == 2:
+   elif type_piano == 2: #piano a queue
         tarif_piano = TARIF_PIANO_A_QUEUE
-    else :
+   else:
         print("erreur")
     
-    for i in range(nombre_km):
+   if (nombre_etage_descendre+nombre_etage_monter) > 10:
+       tarif_total_etage = tarif_piano * 10
+   else:
+       for eta in range(nombre_etage_descendre+nombre_etage_monter):
+           tarif_total_etage += tarif_piano
+    
+   for i in range(nombre_km):
         tarif_total_km += TARIF_KM
         
-    #
-    # WIP
-    #
+   tarif_final = tarif_total_etage+tarif_total_km
+   
+   if tarif_final < 200:
+       tarif_final = 200
+   elif tarif_final >2500:
+       tarif_final = 2500
+        
+   return tarif_final,tarif_total_km
+
+def afficher(type_piano,tarif_total_km,nombre_km,tarif_final):
+    if type_piano == 1:
+        print(f"Prix total à payer pour un piano :{tarif_final}.- (dont {tarif_total_km}.- de transport pour les {nombre_km}km)")
+    elif type_piano == 2:
+        print(f"Prix total à payer pour un piano à queue :{tarif_final}.- (dont {tarif_total_km}.- de transport pour les {nombre_km}km)")
+    
 # Procédure main()
 def main():
     type_piano,nombre_etage_monter,nombre_etage_descendre,nombre_km = entree()
-    
-    
+    tarif_final, tarif_total_km = calculer(type_piano,nombre_etage_monter,nombre_etage_descendre,nombre_km)
+    afficher(type_piano,tarif_total_km,nombre_km,tarif_final)
 # Appel de la procédure main()
 if __name__ == "__main__":
     main()
