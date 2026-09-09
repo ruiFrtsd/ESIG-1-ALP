200bb6edf139604d0fd170d211465504c75083b6
FREITAS DA MOTA RUI MANUEL <RUI.FRTSD@ge-pedago.etat-ge.ch>
2026-09-09 16:58:52 +0200

    s8 added

--- ALP-S8/ALP-S8ExT1.py
+++ ALP-S8/ALP-S8ExT1.py
@@ -1,13 +1,30 @@
+# Imports
 from turtle import *
+# Constantes
 
-for i in range(200):
-    if i % 2 == 0:
-        fd(10 + i)
+
+
+# Procédures et fonctions
+def entree():
+    longeur = int(input("Longeur initial : "))
+    increment = int(input("Increment : "))
+    longeur_max = int(input("Longeur max : "))
+     
+    return longeur,increment,longeur_max
+
+def dessiner(longeur,longeur_max,increment):
+    speed(-1)
+    while longeur <= longeur_max:
+        fd(longeur)
         right(90)
-    else:
-        fd(9 + i)
-        right(90)
+        longeur+=increment
         
-        
-        
-        ##to develop
+# Procédure main()
+def main():
+    longeur,increment,longeur_max = entree()
+    dessiner(longeur,longeur_max,increment)
+    
+# Appel de la procédure main()
+if __name__ == "__main__":
+    main()
+
